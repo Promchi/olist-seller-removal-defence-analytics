09fa25e9e6827ecd006261d7cc27e374d1e826ee
Promise Ezeike <ezeikechime25@gmail.com>
2026-03-29 19:20:08 +0100

    Update and rename db_connection.py to Scripts/db_connection.py

--- Scripts/db_connection.py
+++ Scripts/db_connection.py
@@ -4,8 +4,8 @@ def get_connection():
     conn = psycopg2.connect(
         host="localhost",
         database="OList",
-        user="postgres",
-        password="chiemela",
+        user="your_username",
+        password="your_password",
         port="5432"
     )
     return conn
